4b99cce4b9464048d746a56061a63ea06b1fef6f
Sally Mohamed Ali <120983369+sally182@users.noreply.github.com>
2023-09-07 01:02:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# Define the FastAPI application
+from typing import List, Union
+from typing_extensions import Annotated
+from fastapi import FastAPI, Query
+import joblib
+import numpy as np
+
+
+def scale_and_predict(data):
+    scaler = joblib.load("scaler.gz")
+    model = joblib.load("model.gz")
+    data = np.array(data)
+    print(data)
+    scaled_data = scaler.transform(data)
+    pred = model.predict([scaled_data])
+    return pred
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/predict")
+async def predict(q: Annotated[Union[List[float], None], Query()] = None):
+    data=q
+    #prediction = scale_and_predict(data)
+    return {"prediction": data}
